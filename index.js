124a27c8e84374d24dce0761467dcaa75a7e38d7
Bertil <30950082@qq.com>
2022-11-19 14:13:50 +0800

    feat: add store

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 import { registerMicroApps, start } from "./src/start"
-export { registerMicroApps, start }
+import { creatStore } from "./src/store/store"
+export { registerMicroApps, start, creatStore }
